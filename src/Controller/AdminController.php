669f88c7bb505e9e22f062c593946afb19c214e9
David Duggins <weatheredwatcher@gmail.com>
2020-07-31 01:03:06 -0400

    added admin dashboards for users and posts

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,6 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Blog;
+use App\Form\BlogType;
+use App\Repository\BlogRepository;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
 
 class AdminController extends AbstractController
 {
@@ -14,6 +21,32 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController', 'page' => 'admin_main'
+        ]);
+    }
+
+     /**
+     * @Route("/admin/users", name="admin_users")
+     */
+    public function showUsers()
+    {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $users = $repository->findAll();
+
+        return $this->render('admin/users.html.twig', [
+            'controller_name' => 'AdminController', 'page' => 'admin_main', 'users' => $users
+        ]);
+    }
+
+     /**
+     * @Route("/admin/posts", name="posts")
+     */
+    public function showPosts()
+    {
+        $repository = $this->getDoctrine()->getRepository(Blog::class);
+        $posts = $repository->findAll();
+
+        return $this->render('admin/posts.html.twig', [
+            'controller_name' => 'AdminController', 'page' => 'admin_main', 'posts' => $posts
         ]);
     }
 }
